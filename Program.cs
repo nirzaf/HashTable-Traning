0798759cb5b83ad8774ad7b1e52b0945a50e29bf
M F M Fazrin <mfmfazrin1986@gmail.com>
2021-11-27 22:35:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+using Bogus;
+
+Console.WriteLine("Welcome to Learn about Hash Table using C#");
+
+var myHashtable = new Hashtable();
+
+
+for (int i = 1; i < 100; i++)
+{
+    var myFaker = new Faker();
+    myHashtable.Add(i, myFaker.Address.City());
+}
+
+myHashtable.Remove(50);
+
+foreach (DictionaryEntry hTable in myHashtable)
+{
+    Console.WriteLine($"Key: {hTable.Key} ,  Values: {hTable.Value}");
+}
+
+
+Console.ReadLine();
